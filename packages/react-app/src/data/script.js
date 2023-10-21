3c03cbd34b46d77ff219c84fd1648e2d27646a78
namangarg1902 <115886209+namangarg1902@users.noreply.github.com>
2023-10-22 02:37:42 +0530

    Added Block Explorer

--- packages/react-app/src/data/script.js
+++ packages/react-app/src/data/script.js
@@ -1,40 +1,121 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
+const jsdom = require('jsdom');
 
-async function getEthereumGasFees() {
+const { JSDOM } = jsdom;
+async function scrapeWebPageEth(url) {
   try {
-    const response = await axios.get('https://api.etherscan.io/api', {
-      params: {
-        module: 'gastracker',
-        action: 'gasoracle',
-        apikey: 'WCAHBVAXM5VM9F144YFU1F79BUFUJ6JGI9', // Replace with your Etherscan API key
-      },
-    });
+    const response = await fetch(url);
 
-    const gasFeeData = response.data.result;
-    
-    // Extract and return the gas fees
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the page. Status code: ${response.status}`);
+    }
+
+    const html = await response.text();
+    const { window } = new JSDOM(html);
+    const lp = window.document.querySelector('#spanLowPrice').textContent;
+    const ap = window.document.querySelector('#spanAvgPrice').textContent;
+    const hp = window.document.querySelector('#spanHighPrice').textContent;
+
+    const ans = (parseInt(lp , 10) + parseInt(ap , 10) + parseInt(hp , 10))/3;
+
     return {
-      safeGasPrice: gasFeeData.SafeGasPrice / 1e9,
-      proposeGasPrice: gasFeeData.ProposeGasPrice /1e9,
-      fastGasPrice: gasFeeData.FastGasPrice/1e9,
+      ans
     };
   } catch (error) {
-    console.error('Error fetching Ethereum gas fee data:', error);
+    console.error('Error scraping the web page:', error);
     return null;
   }
 }
 
-async function main() {
-  const ethereumGasFees = await getEthereumGasFees();
+async function scrapeWebPagePoly(url) {
+  try {
+    const response = await fetch(url);
 
-  if (ethereumGasFees !== null) {
-    console.log('Ethereum Gas Fees:');
-    console.log('SafeGasPrice (Rs):', ethereumGasFees.safeGasPrice * 50000);
-    console.log('ProposeGasPrice (Rs):', ethereumGasFees.proposeGasPrice * 50000);
-    console.log('FastGasPrice (Rs):', ethereumGasFees.fastGasPrice * 50000) ;
-  } else {
-    console.log('Failed to fetch Ethereum gas fee data.');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the page. Status code: ${response.status}`);
+    }
+
+    const html = await response.text();
+    const { window } = new JSDOM(html);
+    const lp = window.document.querySelector('#standardgas').textContent;
+    const ap = window.document.querySelector('#fastgas').textContent;
+    const hp = window.document.querySelector('#rapidgas').textContent;
+
+    const ans = (parseInt(lp , 10) + parseInt(ap , 10) + parseInt(hp , 10))/3;
+
+    return {
+      ans
+    };
+  } catch (error) {
+    console.error('Error scraping the web page:', error);
+    return null;
   }
 }
 
-main()
+
+async function scrapeWebPageAva(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the page. Status code: ${response.status}`);
+    }
+
+    const html = await response.text();
+    const { window } = new JSDOM(html);
+    const lp = window.document.querySelector('#standardgas').textContent;
+    const ap = window.document.querySelector('#fastgas').textContent;
+    const hp = window.document.querySelector('#rapidgas').textContent;
+
+    const ans = (parseInt(lp , 10) + parseInt(ap , 10) + parseInt(hp , 10))/3;
+
+    return {
+      ans
+    };
+  } catch (error) {
+    console.error('Error scraping the web page:', error);
+    return null;
+  }
+}
+
+// Usage example
+async function main() {
+  const urlEth = 'https://etherscan.io/gastracker';
+  const urlPoly = 'https://polygonscan.com/gastracker'
+  const urlAva = 'https://snowtrace.io/gastracker';
+  const scrapedDataEth = await scrapeWebPageEth(urlEth);
+  const scrapedDataPoly = await scrapeWebPagePoly(urlPoly);
+  const scrapedDataAva = await scrapeWebPageAva(urlAva);
+
+
+  if (scrapedDataPoly !== null && scrapedDataAva && scrapedDataEth) {
+    const a = scrapedDataEth.ans;
+    const b = scrapedDataPoly.ans;
+    const c = scrapedDataAva.ans ;
+    var ans ;
+    if(a < b && a<c){
+      console.log("Eth is having lowest Congestion")
+      ans = a
+    }
+    else if(b < a && b<c){
+      console.log("Poly is having lowest Congestion")
+      ans = b
+    }
+    else{
+      console.log("Ava is having lowest Congestion")
+      ans = 3
+    }
+    return{
+      ans,
+    }
+
+  } else {
+    console.log('Failed to scrape the web page.');
+    return null ;
+  }
+}
+
+const ans = main();
+
+// export default ans ;
+module.exports = ans;
